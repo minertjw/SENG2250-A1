355eda9d0bdb06c219190ddebc281427575afb32
minertjw <thomas@wood-lands.com>
2024-08-23 01:38:43 +1000

    Operational 512 bit plaintext encryptor

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import os
 import random
-import json
 
 from Crypto.Cipher import AES
 from Crypto.Cipher.AES import MODE_ECB, MODE_CTR
-from base64 import b64encode, b64decode
+from convert import BinaryToHex, StringToBinary, HexToBinary
 
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls')
 
 random.seed(1)
+
+def printNonce(nonce):
+    for i in nonce:
+        binary = "{:08d}".format(int(bin(i).split('b')[1]))
+        print(BinaryToHex(binary),end="")
+    print("")
+
+def ByteToBit(arr):
+    bits = ""
+    for i in arr:
+        bits += "{:08d}".format(int(bin(i).split("b")[1]))
+    return bits
+
+def XOR(outNonce, chunk):
+    outNonceBits = ByteToBit(outNonce)
+    chunkBits = ByteToBit(chunk)
+    result = ""
+    for i in range(len(chunkBits)):
+        if outNonceBits[i] != chunkBits[i]:
+            result += str(1)
+        else:
+            result += str(0)
+    # print(outNonceBits)
+    # print(chunkBits)
+    # print(result)
+    return result
 
 # 256-bit key (32 bytes)
 # Storing as a hexadecimal number, but need to convert to string and then encode as bytes
@@ -16,37 +41,48 @@ random.seed(1)
 hex_key = 'C3376601427A45DBD2AD34DBCD5325E25E0305F48B3A9A3FCB80E861858AB249'
 byte_key = bytearray.fromhex(hex_key)
 
-mode = MODE_ECB
-mode = MODE_CTR
-
-# Nonce length in bytes
-nonce_length = 64
+# 512-bit plaintext (64 bytes)
+plaintext = "I wonder how many letters I have to work with? Heaps. Very good."
+byte_plaintext = bytearray(plaintext.encode())
 
 # Create a nonce randomly
+# Size of nonce is 8 bits * 16 = 128 bits (16 bytes)
+nonce_length = 16
 temp_array = []
-for i in range(nonce_length/8):
+for i in range(nonce_length):
     temp_array.append(random.randint(0,255))
 nonce = bytearray(temp_array)
 
-initial_value = 0
+print("Entire Plaintext:", StringToBinary(plaintext))
+print("Key:", HexToBinary(hex_key))
+print("IV:", ByteToBit(nonce))
 
-# 512-bit plaintext (64 bytes)
-plaintext = b'I wonder how many letters I have to work with? Heaps. Very good.'
+# Define cipher
+cipher = AES.new(key=byte_key, mode=MODE_ECB)
 
+# print(len(byte_key)*8)
+# print(len(plaintext)*8)
+# print(len(nonce)*8)
 
-cipher = AES.new(key=byte_key, mode=MODE_CTR, nonce=nonce, initial_value=initial_value, counter=None)
+ciphertext = ""
+counter = 1
+for i in range(0, len(byte_plaintext), len(nonce)):
+    print("\nBlock ", counter, ":", sep="")
+    print("    Input of AES:", ByteToBit(nonce))
+    outNonce = cipher.encrypt(nonce)
+    print("    Output of AES:", ByteToBit(outNonce))
+    chunk = byte_plaintext[i:i+len(nonce)]
+    # print(chunk)
+    result = XOR(outNonce, chunk)
+    print("    Result of XOR:", result)
+    ciphertext += result
+    nonce[-1]+=1
 
-returned_nonce = cipher.nonce
+print("\nEntire Ciphertext:", ciphertext)
 
-ct_bytes = cipher.encrypt(plaintext)
-nonce = b64encode(cipher.nonce).decode('utf-8')
-ct = b64encode(ct_bytes).decode('utf-8')
-result = json.dumps({'nonce':nonce, 'ciphertext':ct})
-print(result)
+# plaintext = "A good Plaintext"
+# print(StringToBinary(plaintext))
+# print(len(StringToBinary(plaintext)))
 
-try:
-    cipher = AES.new(byte_key, MODE_CTR, nonce=b64decode(nonce))
-    pt = cipher.decrypt(b64decode(ct))
-    print("The message was:", pt)
-except (ValueError, KeyError):
-    print("Incorrect decryption")
+# cipher = AES.new(key=byte_key, mode=MODE_ECB)
+
