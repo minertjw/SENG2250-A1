429ee68c31bb38842b588c346df2fe41cc33e06a
minertjw <thomas@wood-lands.com>
2024-08-23 13:23:34 +1000

    Fully working

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import os
 import random
 
 from Crypto.Cipher import AES
-from Crypto.Cipher.AES import MODE_ECB, MODE_CTR
-from convert import BinaryToHex, StringToBinary, HexToBinary
+from Crypto.Cipher.AES import MODE_ECB
+from convert import BinaryToHex, StringToBinary, HexToBinary, StringToHex
 
 os.system('cls')
 
 random.seed(1)
 
-def printNonce(nonce):
-    for i in nonce:
+def printIV(iv):
+    for i in iv:
         binary = "{:08d}".format(int(bin(i).split('b')[1]))
         print(BinaryToHex(binary),end="")
+    print("")
+
+def nicePrintBits(byteString):
+    for i in range(0,len(byteString), 8):
+        print(byteString[i:i+8],end=" ")
     print("")
 
 def ByteToBit(arr):
@@ -21,24 +26,21 @@ def ByteToBit(arr):
         bits += "{:08d}".format(int(bin(i).split("b")[1]))
     return bits
 
-def XOR(outNonce, chunk):
-    outNonceBits = ByteToBit(outNonce)
+def XOR(outIV, chunk):
+    outIVBits = ByteToBit(outIV)
     chunkBits = ByteToBit(chunk)
     result = ""
     for i in range(len(chunkBits)):
-        if outNonceBits[i] != chunkBits[i]:
+        if outIVBits[i] != chunkBits[i]:
             result += str(1)
         else:
             result += str(0)
-    # print(outNonceBits)
-    # print(chunkBits)
-    # print(result)
     return result
 
 # 256-bit key (32 bytes)
 # Storing as a hexadecimal number, but need to convert to string and then encode as bytes
 # Size of key is 64 characters * 4 bits = 256 bits (32 bytes)
-hex_key = 'C3376601427A45DBD2AD34DBCD5325E25E0305F48B3A9A3FCB80E861858AB249'
+hex_key = 'c3376601427a45dbd2ad34dbcd5325e25e0305f48b3a9a3fcb80e861858ab249'
 byte_key = bytearray.fromhex(hex_key)
 
 # 512-bit plaintext (64 bytes)
@@ -48,41 +50,93 @@ byte_plaintext = bytearray(plaintext.encode())
 # Create a nonce randomly
 # Size of nonce is 8 bits * 16 = 128 bits (16 bytes)
 nonce_length = 16
-temp_array = []
+nonce_array = []
 for i in range(nonce_length):
-    temp_array.append(random.randint(0,255))
-nonce = bytearray(temp_array)
+    nonce_array.append(random.randint(0,255))
+iv = bytearray(nonce_array)
 
-print("Entire Plaintext:", StringToBinary(plaintext))
-print("Key:", HexToBinary(hex_key))
-print("IV:", ByteToBit(nonce))
+print("Entire Plaintext:", StringToHex(plaintext))
+print("Key:", hex_key)
+print("IV:", BinaryToHex(ByteToBit(iv)))
 
 # Define cipher
 cipher = AES.new(key=byte_key, mode=MODE_ECB)
 
-# print(len(byte_key)*8)
-# print(len(plaintext)*8)
-# print(len(nonce)*8)
-
 ciphertext = ""
 counter = 1
-for i in range(0, len(byte_plaintext), len(nonce)):
+for i in range(0, len(byte_plaintext), len(iv)):
     print("\nBlock ", counter, ":", sep="")
-    print("    Input of AES:", ByteToBit(nonce))
-    outNonce = cipher.encrypt(nonce)
-    print("    Output of AES:", ByteToBit(outNonce))
-    chunk = byte_plaintext[i:i+len(nonce)]
+    print("    Input of AES: ", BinaryToHex(ByteToBit(iv)))
+    outIV = cipher.encrypt(iv)
+    print("    Output of AES:", BinaryToHex(ByteToBit(outIV)))
+    chunk = byte_plaintext[i:i+len(iv)]
     # print(chunk)
-    result = XOR(outNonce, chunk)
-    print("    Result of XOR:", result)
+    result = XOR(outIV, chunk)
+    print("    Result of XOR:", BinaryToHex(result))
     ciphertext += result
-    nonce[-1]+=1
+    iv[-1]+=1
 
-print("\nEntire Ciphertext:", ciphertext)
+print("\nEntire Ciphertext:", BinaryToHex(ciphertext),"\n\n\n")
 
-# plaintext = "A good Plaintext"
-# print(StringToBinary(plaintext))
-# print(len(StringToBinary(plaintext)))
 
-# cipher = AES.new(key=byte_key, mode=MODE_ECB)
+# five unique 1-bit flips
+plaintext = "A good Plaintext"
+byte_plaintext = bytearray(plaintext.encode())
 
+# nicePrintBits(ByteToBit(byte_plaintext))
+
+cipher = AES.new(key=byte_key, mode=MODE_ECB)
+
+original_ciphertext = ByteToBit(cipher.encrypt(byte_plaintext))
+
+ciphertext_list = []
+swap_position = 0
+counter = 1
+for j in range(5):
+    new_plaintext = ""
+    for i in range(0,len(ByteToBit(byte_plaintext))):
+        if (i == swap_position):
+            if (ByteToBit(byte_plaintext)[i] == "0"):
+                new_plaintext += "1"
+            else:
+                new_plaintext += "0"
+        else:
+            new_plaintext += ByteToBit(byte_plaintext)[i]
+    print("1-bit flip ", counter,sep="")
+    print("Original Plaintext: ",end="")
+    nicePrintBits(ByteToBit(byte_plaintext))
+    print("Swap Index =",swap_position,":    ", end="") 
+    for i in range(swap_position+int(swap_position/8)):
+        print(" ",end="")
+    print(u'\u2193')
+    swap_position += int(len(ByteToBit(byte_plaintext))/5)
+    print("Altered Plaintext:  ",end="")
+    nicePrintBits(new_plaintext)
+    new_byte_plaintext = bytearray.fromhex(BinaryToHex(new_plaintext))
+    ciphertext = cipher.encrypt(new_byte_plaintext)
+    #nicePrintBits(ByteToBit(ciphertext))
+    print("")
+    ciphertext_list.append(ByteToBit(ciphertext))
+    counter+=1
+
+ciphertext = cipher.encrypt(byte_plaintext)
+
+# print(ciphertext_list)
+counter = 1
+for variant_ciphertext in ciphertext_list:
+    differences = 0
+    difference_positions = ""
+    for j in range(len(variant_ciphertext)):
+        if original_ciphertext[j] != variant_ciphertext[j]:
+            differences+=1
+            difference_positions+=u'\u2193'
+        else:
+            difference_positions+=" "
+        if (j+1)%8==0:
+            difference_positions+=" "
+    print("Number of altered bits ",counter, " = ",differences,sep="")
+    nicePrintBits(original_ciphertext)
+    print(difference_positions)
+    nicePrintBits(variant_ciphertext)
+    print("")
+    counter+=1
